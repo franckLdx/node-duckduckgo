25989f2a4d024e503c1b4796ab9e0a8eb9170b91
FranckLdx <franck.ledoux.pro@gmail.com>
2016-12-11 18:19:00 +0100

    Use my gulp-use-tsconfig plugin

--- gulpfile.js
+++ gulpfile.js
@@ -7,71 +7,41 @@ const tslint = require('gulp-tslint');
 const typeScript = require('gulp-typeScript');
 const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 const path = require('path');
+const useTsConfig = require('gulp-use-tsconfig');
 
-class TsConfig {
-    constructor(tsConfigLocation) {
-      this._location = path.dirname(path.resolve('.', tsConfigLocation));
-      this._tsConfig = require(tsConfigLocation);
-    }
-
-    get tsDir() { return `${this._location}/${this._tsConfig.rootDir}`; }
-
-    get tsFiles() { return `${this.tsDir}/**/*.ts`; }
-
-    get jsDir() { return `${this._location}/${this._tsConfig.compilerOptions.outDir}`; }
-
-    get jsFiles() { return `${this.jsDir}/**/*.js`; }
-
-    get mapFiles() { return `${this.jsDir}/**/*.map`; }
-
-    get ambiantFiles() { return `${this.jsDir}/**/*.d.ts`; }
-
-    get compilerOptions() { return this._tsConfig.compilerOptions; }
-
-    get isSourceMap() { return this._tsConfig.compilerOptions.sourceMap || false; }
-
-    cleanBuildTask() {
-      return del([this.jsFiles, this.mapFiles, this.ambiantFiles]);
-    }
-
-    buildTask(clean=true) {
-      if (clean) {
-        this.cleanBuildTask();
-      }
-      const isSourcemap = this.isSourceMap;
-      let pipe = gulp.src(this.tsFiles);
-      if (isSourcemap) {
-        pipe = pipe.pipe(sourcemaps.init());
-      }
-      pipe = pipe.pipe(typeScript(this.compilerOptions))
-      if (isSourcemap) {
-        pipe = pipe.pipe(sourcemaps.write('.'));
-      }
-      return pipe.pipe(gulp.dest(this.jsDir));
-    }
-}
-
-const libTsConfig = new TsConfig('./src/tsconfig.json');
-const testTsConfig = new TsConfig('./test/tsconfig.json');
+const libTsConfig = './src/tsconfig.json';
+const testTsConfig = './test/tsconfig.json';
 
 const TEST_CONFIG = {
   coverageDir : './coverage',
 }
 
 gulp.task('lib:lint', function() {
-  return gulp.src(libTsConfig.tsFiles)
+  return gulp.src('./src/**/*.ts')
     .pipe(tslint({
       formatter: "verbose"
     }))
     .pipe(tslint.report())
 });
 
-gulp.task('lib:build', ['lib:lint'], () => {
-  return libTsConfig.buildTask();
+gulp.task('lib:clean', () => {
+  return gulp.src(libTsConfig)
+    .pipe(useTsConfig.build());
+});
+
+gulp.task('lib:build', ['lib:clean', 'lib:lint'], () => {
+  return gulp.src(libTsConfig)
+    .pipe(useTsConfig.build());
+});
+
+gulp.task('test:clean', () => {
+  return gulp.src(testTsConfig)
+    .pipe(useTsConfig.build());
 });
 
 gulp.task('test:build', ['lib:build'], () => {
-  return testTsConfig.buildTask();
+  return gulp.src(testTsConfig)
+    .pipe(useTsConfig.build());
 });
 
 gulp.task('test:coverage:clean', function() {
@@ -79,13 +49,13 @@ gulp.task('test:coverage:clean', function() {
 });
 
 gulp.task('test:instrument', ['test:build'], function () {
-  return gulp.src(libTsConfig.jsFiles)
+  return gulp.src('/lib/**/*.js')
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['test:coverage:clean', 'test:instrument'], function () {
-  return gulp.src(testTsConfig.jsFiles)
+  return gulp.src('/lib/**/*.js')
     .pipe(mocha())
     .pipe(istanbul.writeReports({
       reporters: [ 'json' ],
